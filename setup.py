07edbb9a76d56febef0c86bbf7d068f73697cc39
GSelvakumar <iselvakumarg@outlook.com>
2024-07-16 08:12:57 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Information Retrieval GENAI",
+    version="0.0.0",
+    author="Selvakumar",
+    author_email="iselvakumarg@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
